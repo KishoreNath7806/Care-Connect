55bc64127a0ba659c482aa200a64314ea2e21528
Kishore Nath <kishorenath2308@gmail.com>
2025-10-14 21:47:25 +0530

    Day-3 DB Table creation and Integration

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,8 +10,10 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import { Button } from "./ui/button";
+import { checkUser } from "@/lib/checkUser";
 
-export default function(){
+export default async function(){
+    await checkUser();  
     return(
         <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-20 supports-[backdrop-filter]:bg-background/60">
             <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
